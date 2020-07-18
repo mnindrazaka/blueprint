a1546d85000e131a59a82b1ae5d3fc4885ab8ff4
muhammadshiva <62060912+muhammadshiva@users.noreply.github.com>
2020-07-18 15:21:32 +0700

    featImplementasi UI Delete Project (#10)
    * Implementasi UI Delete Project
    
    * Fix UI Delete Project
    
    * Latest fix UI delete project

--- src/modules/LoggedIn/Project/ProjectList.tsx
+++ src/modules/LoggedIn/Project/ProjectList.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
-import { Card, Row, Col } from 'antd'
+import { Card, Row, Col, message } from 'antd'
 import { useProjectSync } from './useProjectSync'
+import { Popconfirm } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
 
 const ProjectList = () => {
+  const confirm = React.useCallback(() => {
+    message.success('Task Deleted');
+  }, [])
+
+  const cancel = React.useCallback(() => {
+    message.error('Canceled');
+  }, [])
+
   const projectSync = useProjectSync()
   return (
     <Row style={{ marginTop: 16 }} gutter={[24, 16]}>
@@ -25,7 +35,25 @@ const ProjectList = () => {
         projectSync.projects.map((project) => {
           return (
             <Col span={6} key={project._id}>
-              <Card hoverable>
+              <Card hoverable
+                actions={[
+                  <Popconfirm
+                    title="Are you sure delete this project ?"
+                    onConfirm={confirm}
+                    onCancel={cancel}
+                    okText="Yes"
+                    cancelText="No"
+                  >
+                    <a href="#">
+                      <DeleteOutlined
+                        style={{
+                          fontSize: 20
+                        }}
+                      />
+                    </a>
+                  </Popconfirm>
+                ]}
+              >
                 <Card.Meta
                   title={project.name}
                   description={project.description}
